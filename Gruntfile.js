f7a1a9b86a621e21dc6d66437f2c08003ac3dd3f
Yile Liu <yile.liu@autodesk.com>
2017-05-11 15:16:51 +0800

    add test files

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'upstream',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: false,
+                metadata: '',
+                regExp: false
+            }
+        },
+    })
+}
